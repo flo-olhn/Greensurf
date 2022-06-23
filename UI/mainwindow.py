40f09f18ea3c03f4adab988d68a28e615507a340
Foxtr0-t <codes.flo@gmail.com>
2022-06-23 04:05:23 +0200

    final project

--- UI/mainwindow.py
+++ UI/mainwindow.py
@@ -1,11 +1,15 @@
 # This Python file uses the following encoding: utf-8
 import sys
-
+from datetime import timedelta
 from PySide6.QtWidgets import QApplication, QMainWindow
 from PyQt6 import QtCore
+from PySide6.QtGui import QIcon
 from ui_mainwindow import Ui_MainWindow
 import psutil
 import pyqtgraph as pg
+from emissions_tracker import EmissionsTracker
+
+
 
 
 class NetworkTraffic():
@@ -23,17 +27,29 @@ class NetworkTraffic():
         self.co2eq += ((bytes_total - self.starting_bytes_nb) * 1.52E-10) * 0.055
         self.starting_bytes_nb = bytes_total
         return self.co2eq
+        
+
+class HardwareEmission():
+
+    def __init__(self):
+        self.hardware_tracker = EmissionsTracker()
+
+    def emissions(self):
+        self.hardware_tracker.start()
+        val = self.hardware_tracker.stop()
+        return val
 
 
 class Graph():
 
     def __init__(self) -> None:
-        self.x_values = [0 for i in range(86400)]
-        self.y_values = [0 for i in range(86400)]
+        self.x_values = [0 for _ in range(86400)]
+        self.y_values = [0 for _ in range(86400)]
         self.pen = pg.mkPen(color=(94, 255, 125))
         self.data_line = []
+        self.data_line2 = []
         self.pen = pg.mkPen(color=(94, 255, 125))
-        # self.data_line = MainWindow.ui.networkGraph.plot([],[], fillLevel = min(self.y_values), brush=pg.mkBrush(110, 255, 165, 10), pen=self.pen)
+        # print(cpuname(), gpuname())
 
     def updatePlotData(self, graph_ui, graph_type):
         self.x_values = self.x_values[1:]
@@ -41,18 +57,12 @@ class Graph():
         self.y_values = self.y_values[1:]
         if (self.x_values[-1] + 1) % 3600 == 0:
             graph_ui.setXRange(self.x_values[-1], self.x_values[-1] + 3600)
-            #self.ui.hardwareGraph.setXRange(self.x_values[-1], self.x_values[-1] + 3600)
         m = max(self.y_values)
-    
-        # modification to do -> what if i need to plot on hardware graph instead???
         self.y_values.append(graph_type.emissions())
-
-
         if (m < max(self.y_values)) and (self.x_values[-1] + 1 > 3600): 
             graph_ui.setYRange(self.y_values[-3600],max(self.y_values))
-            #self.ui.hardwareGraph.setYRange(self.y_values[-3600],max(self.y_values))
         self.data_line.setData(self.x_values, self.y_values)
-        #self.data_line2.setData(self.x_values, self.y_values)
+        #emission_fun()
 
 
 class MainWindow(QMainWindow):
@@ -64,11 +74,13 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.setStyleSheet("background-color: #101217;")
+        self.setWindowIcon(QIcon("C:/Users/ouilh/OneDrive/Documents/Projet_E3/Greensurf/UI/assets/greensurf_icon.ico"))
 
         # Initialisation obj NetworkTraffic and Graph
         self.net_traffic = NetworkTraffic()
+        self.hardware = HardwareEmission()
         self.net_graph = Graph()
+        self.hardware_graph = Graph()
 
         self.ui.startButton.clicked.connect(self.start)
         self.ui.stopButton.clicked.connect(self.stop)
@@ -78,24 +90,40 @@ class MainWindow(QMainWindow):
         self.ui.networkGraph.showGrid(x=True, y=True, alpha=0.1)
         self.ui.hardwareGraph.showGrid(x=True, y=True, alpha=0.1)
         self.ui.hardwareGraph.setBackground('#101217')
+        self.ui.stopButton.setEnabled(False)
         
         self.pen = pg.mkPen(color=(94, 255, 125))
         self.net_graph.data_line = self.ui.networkGraph.plot([],[], fillLevel = min(self.net_graph.y_values), brush=pg.mkBrush(110, 255, 165, 10), pen=self.net_graph.pen)
-        self.data_line2 = self.ui.hardwareGraph.plot([], [], pen=self.pen)
+
+        self.hardware_graph.data_line = self.ui.hardwareGraph.plot([],[], fillLevel = min(self.hardware_graph.y_values), brush=pg.mkBrush(110, 255, 165, 10), pen=self.hardware_graph.pen)
 
         self.timer = QtCore.QTimer()
         self.timer.setInterval(1000)
 
         self.timer.timeout.connect(lambda: self.net_graph.updatePlotData(self.ui.networkGraph, self.net_traffic))
-        #self.timer.timeout.connect(self.updatePlotData)
+        self.timer.timeout.connect(lambda: self.hardware_graph.updatePlotData(self.ui.hardwareGraph, self.hardware))
+        self.timer.timeout.connect(self.updateUI)
+
+    def updateUI(self):
+        self.ui.label_duration.setText(str(timedelta(0, self.net_graph.x_values[-1])))
+        self.ui.label_total_emission.setText(str("{:.4e}".format(self.net_graph.y_values[-1] + self.hardware_graph.y_values[-1])))
+        self.ui.label_car_km.setText(str("{:.4e}".format(float(self.ui.label_total_emission.text()) / 0.2)))
+        self.ui.label_train_km.setText(str("{:.4e}".format(float(self.ui.label_total_emission.text()) / 0.002)))
+        self.ui.label_plane_km.setText(str("{:.4e}".format(float(self.ui.label_total_emission.text()) / 0.1)))
 
     def start(self):
         self.timer.start()
+        self.ui.startButton.setEnabled(False)
+        self.ui.stopButton.setEnabled(True)
 
     def stop(self):
         self.timer.stop()
         self.net_graph.x_values = [0 for _ in range(86400)]
         self.net_graph.y_values = [0 for _ in range(86400)]
+        self.hardware_graph.x_values = [0 for _ in range(86400)]
+        self.hardware_graph.y_values = [0 for _ in range(86400)]
+        self.ui.startButton.setEnabled(True)
+        self.ui.stopButton.setEnabled(False)
     
 
 if __name__ == "__main__":
